b7c486928037fd65d4db593802351430db13ec7c
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-11-13 19:37:07 +0530

    switched from class to struct. since all members are public moved from class to struct.

--- Assignment-1/shared.h
+++ Assignment-1/shared.h
@@ -1,9 +1,8 @@
 /*Class defination to be included. To reduce code duplication adding it to a header file.*/
-class sharedclass
+struct sharedclass
 {
-    public:
-        int add (int, int);
-        int subtract (int, int);
-        int multiply (int, int);
-        int divide (int, int);
+    int add (int, int);
+    int subtract (int, int);
+    int multiply (int, int);
+    int divide (int, int);
 };
