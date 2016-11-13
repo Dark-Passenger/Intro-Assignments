19bc66d0e7b23a2b4ec7bd29885f32b25b0101af
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-11-13 22:05:40 +0530

    Code refactoring

--- Assignment-11/database_handling.cpp
+++ Assignment-11/database_handling.cpp
@@ -8,84 +8,84 @@ using namespace std;
 class Db_Handler
 {
 private:
-	MYSQL *connect = mysql_init(NULL);
-	MYSQL_RES *result;
-	MYSQL_ROW row;
-	char *server = "server";
-	char *user = "username";
-	char *password = "password";
-	char *database = "local";
+    MYSQL *connect = mysql_init(NULL);
+    MYSQL_RES *result;
+    MYSQL_ROW row;
+    char *server = "server";
+    char *user = "username";
+    char *password = "password";
+    char *database = "local";
 
 string querybuilder(vector<int> val, string table_header)
 {
-	string query("INSERT INTO ");
-	string.append(table_header);
-	 string.append("VALUES ");
+    string query("INSERT INTO ");
+    string.append(table_header);
+     string.append("VALUES ");
 
-	for( vector<int>::iterator itr = val.begin() ; itr != val.end(); ++itr)
-	{
-		query.append("(");
-    	query.append(itr);
-    	query.append(")");
-    	if(it+1 != values.end() )
-    		{
-    			query.append(",");
-    		}
-	}
-	query.append(";");
-	
-	return query;
+    for( vector<int>::iterator itr = val.begin() ; itr != val.end(); ++itr)
+    {
+        query.append("(");
+        query.append(itr);
+        query.append(")");
+        if(it+1 != values.end() )
+            {
+                query.append(",");
+            }
+    }
+    query.append(";");
+
+    return query;
 }
 
-void writetoDB(	vector<int> numbers)
+void writetoDB( vector<int> numbers)
 {
-	mysql_real_connect(connect, server, user, password, database, 0, NULL, 0);
-	cout << "writing to database...";
-	mysql_query(connect, "(CREATE TABLE example_table (number INT);");
-	query = querybuilder(numbers,"example_table(number)");
-	query_state=mysql_query(connect, query.c_str() );
-	cout << "Query status : " << query_state;
-	result = mysql_use_result(connect);
-	while ((row = mysql_fetch_row(result)) != NULL)
-	{
-		cout << row;		//todo: consider row[1] only
-	}
-	mysql_free_result(result);
-	mysql_close(connect);
+    mysql_real_connect(connect, server, user, password, database, 0, NULL, 0);
+    cout << "writing to database...";
+    mysql_query(connect, "(CREATE TABLE example_table (number INT);");
+    query = querybuilder(numbers,"example_table(number)");
+    query_state=mysql_query(connect, query.c_str() );
+    cout << "Query status : " << query_state;
+    result = mysql_use_result(connect);
+    while ((row = mysql_fetch_row(result)) != NULL)
+    {
+        cout << row;        //todo: consider row[1] only
+    }
+    mysql_free_result(result);
+    mysql_close(connect);
 }
 
 void readfromDB()
 {
-	mysql_real_connect(connect, server, user, password, database, 0, NULL, 0);
-	cout << "reading from database ...";
-	mysql_query(connect, "SELECT number FROM example_table");
-	result = mysql_use_result(connect);
-	vector<int> num;
-	while ((row = mysql_fetch_row(result)) != NULL)
-	{
-		num.push_back(row[1]);
-	}
-	mysql_free_result(result);
+    mysql_real_connect(connect, server, user, password, database, 0, NULL, 0);
+    cout << "reading from database ...";
+    mysql_query(connect, "SELECT number FROM example_table");
+    result = mysql_use_result(connect);
+    vector<int> num;
+    while ((row = mysql_fetch_row(result)) != NULL)
+    {
+        num.push_back(row[1]);
+    }
+    mysql_free_result(result);
 
-	sort(num.begin(), num.end());
-	mysql_query(connect, "(CREATE TABLE sorted_table (number INT);");
-	query = querybuilder(num,"sorted_table(number)");
-	query_state=mysql_query(connect, query.c_str() );
-	cout << "Query status : " << query_state;
-	mysql_close(connect);
+    sort(num.begin(), num.end());
+    mysql_query(connect, "(CREATE TABLE sorted_table (number INT);");
+    query = querybuilder(num,"sorted_table(number)");
+    query_state=mysql_query(connect, query.c_str() );
+    cout << "Query status : " << query_state;
+    mysql_close(connect);
 }
 
 };
 
 int main()
 {
-	vector <int> input;
-	for (int i = 1; i < argc; ++i)
-	{
-		input.push_back(argv[i]);
-	}
-	Db_Handler db;
-	db.writetoDB(input);
-	db.readfromDB();
-	return 0;
+    vector <int> input;
+    for (int i = 1; i < argc; ++i)
+    {
+        input.push_back(argv[i]);
+    }
+    Db_Handler db;
+    db.writetoDB(input);
+    db.readfromDB();
+    return 0;
 }
--- Assignment-4/Part-B/signal_handling.cpp
+++ Assignment-4/Part-B/signal_handling.cpp
@@ -10,44 +10,44 @@ bool isSigCaught = false;
 
 void parent_signal_handler(int sig)
 {
-	cout <<"Parent Signal recieved exiting\n";
-	isChildDone = true;
+    cout <<"Parent Signal recieved exiting\n";
+    isChildDone = true;
 }
 
 void child_signal_handler(int sig)
 {
-	int status;
-	int status_code = WEXITSTATUS(status);
-	cout <<"Child exited with status code : " << status_code << "\n";
-	isSigCaught = true;
+    int status;
+    int status_code = WEXITSTATUS(status);
+    cout <<"Child exited with status code : " << status_code << "\n";
+    isSigCaught = true;
 }
 
 int main()
 {
-	pid_t pid;
+    pid_t pid;
 
-	if((pid = fork()) == 0)
-	{
-     	//child action
-		signal(SIGUSR1, child_signal_handler);
-		while(true)
-		{
-			cout <<"Child waiting\n";
-			if(isSigCaught == true)
-				return 0;
-		}
-	}
-	else
-	{
-		//parent action
-		signal(SIGCHLD, parent_signal_handler);
-		sleep(5);
-		kill(pid, SIGUSR1);
-		while(true)
-		{
-			cout <<"Parent waiting\n";
-			if(isChildDone == true)
-				return 0;
-		}
-	}
+    if((pid = fork()) == 0)
+    {
+        //child action
+        signal(SIGUSR1, child_signal_handler);
+        while(true)
+        {
+            cout <<"Child waiting\n";
+            if(isSigCaught == true)
+                return 0;
+        }
+    }
+    else
+    {
+        //parent action
+        signal(SIGCHLD, parent_signal_handler);
+        sleep(5);
+        kill(pid, SIGUSR1);
+        while(true)
+        {
+            cout <<"Parent waiting\n";
+            if(isChildDone == true)
+                return 0;
+        }
+    }
 }
