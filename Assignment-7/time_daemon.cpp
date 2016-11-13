36452bd50dbbd3377f75aff5f6194106083f45a2
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-11-13 19:46:31 +0530

    Convert files from dos2unix and add makefile.

--- Assignment-7/time_daemon.cpp
+++ Assignment-7/time_daemon.cpp
@@ -5,24 +5,21 @@
 #include <string>
 #include <unistd.h>
 
-using namespace std;
-
 int main()
 {
-    ofstream file;
-    string file_name = "/var/log/time.log";
-    int sleep_time = 300;
+    std::ofstream file;
+    std::string const file_name = "/var/log/time.log";
+    int const sleep_time = 300;
 
     daemon(1,1);    //make a daemon
 
     while(true)
     {
-        time_t end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
-        file.open(file_name, ofstream::app);
-        file << ctime(&end_time);
+        time_t end_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        file.open(file_name, std::ofstream::app);
+        file << std::ctime(&end_time);
         file.close();
         sleep(sleep_time);
     }
 
-    return 0;
 }
--- Assignment-7/time_daemon.sh
+++ Assignment-7/time_daemon.sh
@@ -14,18 +14,20 @@ program="time_daemon"
 daemon=/bin/$program
 LOCKFILE=/var/lock/subsys/$program
 
-start() {
-        echo -n "Starting $program : "
-        $daemon
-        touch /var/lock/subsys/$program
-        return $return_value
+start()
+{
+    echo -n "Starting $program : "
+    $daemon
+    touch /var/lock/subsys/$program
+    return $return_value
 }
 
-stop() {
-        echo -n "Shutting down $program : "
-        killproc $daemon
-        rm -f /var/lock/subsys/$program
-        return $return_value
+stop()
+{
+    echo -n "Shutting down $program : "
+    killproc $daemon
+    rm -f /var/lock/subsys/$program
+    return $return_value
 }
 
 case "$1" in
